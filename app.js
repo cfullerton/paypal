34e39524a4b701ccc7f2424c6bca2e9ac61a9b8d
Conner Fullerton <cfullerton@alkamitech.com>
2016-12-01 20:22:58 -0600

    initial

---
+++ app.js
@@ -0,0 +1,28 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "useYourMerchantId",
+  publicKey: "useYourPublicKey",
+  privateKey: "useYourPrivateKey"
+});
+
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+
+app.post("/checkout", function (req, res) {
+  var nonceFromTheClient = req.body.payment_method_nonce;
+  // Use payment method nonce here
+});
+
+gateway.transaction.sale({
+  amount: "10.00",
+  paymentMethodNonce: nonceFromTheClient,
+  options: {
+    submitForSettlement: true
+  }
+}, function (err, result) {
+});
